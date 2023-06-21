7149eeb42ce95663f59edc498e1225b6fa5c9fb0
gabrielrassweiler <gabriel_rassweiler@hotmail.com>
2023-06-21 15:40:14 -0300

    feat(send): envio da mensagem

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
+import * as XLSX from 'xlsx';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World1!';
+  mensagemPadrao: string;
+
+  constructor() {
+    this.mensagemPadrao =
+      'Olá, quem está falando é a Júlia. Sou consultora comercial direto da fábrica aqui do grupo Alvo da Moda.' +
+      ' Você ficou sabendo da oportunidade que estamos oferecendo para as nossas clientes VIP?';
+  }
+
+  async send(mensagem?: string) {
+    console.log('\n- Começando envio -');
+    const rel = XLSX.readFile('src/clientes/RelCliente.xlsx');
+    const planilha = rel.Sheets[rel.SheetNames[0]];
+    const dados = XLSX.utils.sheet_to_json(planilha, { header: 1 });
+    if (!dados) {
+      console.error('Nenhum dado encontrado na planilha');
+      return;
+    }
+    delete dados[0];
+    dados.sort();
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://web.whatsapp.com');
+    await this.delay(20000);
+    console.log('\n -- Conectado com sucesso! -- ');
+
+    for (const cliente of dados) {
+      if (
+        cliente[4].toLowerCase() == 'sim' ||
+        cliente[5].toLowerCase() == 'nao'
+      )
+        continue;
+
+      const phone =
+        cliente[2].toString() + cliente[3].toString().replaceAll('-', '');
+
+      try {
+        await page.goto(
+          'https://web.whatsapp.com/send?phone=+55' +
+            phone +
+            '&text=' +
+            mensagem ?? this.mensagemPadrao,
+        );
+        await this.delay(5000);
+
+        await page.click("span[data-testid='send']");
+        console.log(
+          `\n -- Enviado mensagem [Código cliente: ${cliente[0]}, fone: ${phone}] -- `,
+        );
+        await this.delay(2000);
+      } catch (e) {
+        console.error(
+          `\n -- ERRO AO ENVIAR [Código cliente: ${cliente[0]}, Fone: ${phone}]. Detalhes: ${e.message} -- `,
+        );
+      }
+    }
+
+    console.log('\n- Fim dos envios -');
+    await browser.close();
+  }
+
+  async delay(time: number) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, time);
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as readline from 'readline';
+import { AppService } from './app.service';
+
+async function init() {
+  const service = new AppService();
+
+  // Cria uma interface de leitura para ler a entrada do usuário
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(
+    '\nInformações:' +
+      '\n- Lembra-se de deixar a planilha atualizada antes de enviar e tambem o nome precisa ser RelCliente.xlsx' +
+      '\n- Para enviar mensagens conforme a planilha importada, digite sim e em seguida a mensagem que queira mandar.' +
+      '\n  EXEMPLO: sim, Olá, teste de mensagem' +
+      '\n  OBS: Caso não queira mudar a mensagem, a mensagem padrão está como: ' +
+      service.mensagemPadrao +
+      '\n- Caso queira parar a execução do programa pressione 2x(ctrl + c).' +
+      '\nR: ',
+    async (answer) => {
+      if (answer.toLowerCase().substring(0, 5) === 'sim, ') {
+        await service.send(answer.replace('sim, ', ''));
+      } else if (answer.toLowerCase().substring(0, 3) === 'sim') {
+        await service.send();
+      }
+
+      rl.close(); // Fecha a interface de leitura
+    },
+  );
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  await init();
 }
 bootstrap();
