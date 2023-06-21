feeb7c01395c94f971d2bd1eb33b0ed37bdc5ba7
gabrielrassweiler <gabriel_rassweiler@hotmail.com>
2023-06-21 17:23:38 -0300

    feat(planilha): atualiza planilha

--- src/app.service.ts
+++ src/app.service.ts
@@ -17,21 +17,23 @@ export class AppService {
       console.error('Nenhum dado encontrado na planilha');
       return;
     }
-    delete dados[0];
-    dados.sort();
 
+    // Abre navegador com whatsapp
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://web.whatsapp.com');
     await this.delay(20000);
     console.log('\n -- Conectado com sucesso! -- ');
 
+    let index = 0;
     for (const cliente of dados) {
-      if (
-        cliente[4].toLowerCase() == 'sim' ||
-        cliente[5].toLowerCase() == 'nao'
-      )
-        continue;
+      // Ignora o primeira pois sao apenas colunas de identificao da planilha
+      if (cliente[0].toString().toLowerCase() == 'codcli') continue;
+
+      index++;
+      if (!cliente) continue;
+      if (cliente[4] && cliente[4].toLowerCase() == 'sim') continue;
+      if (cliente[5] && cliente[5].toLowerCase() == 'nao') continue;
 
       const phone =
         cliente[2].toString() + cliente[3].toString().replaceAll('-', '');
@@ -49,16 +51,42 @@ export class AppService {
         console.log(
           `\n -- Enviado mensagem [Código cliente: ${cliente[0]}, fone: ${phone}] -- `,
         );
+        // Atualiza coluna se foi enviado ou nao
+        dados[index][4] = 'sim';
+        // Atualiza coluna se foi numero esta correto ou nao
+        dados[index][5] = 'sim';
+
         await this.delay(2000);
       } catch (e) {
         console.error(
           `\n -- ERRO AO ENVIAR [Código cliente: ${cliente[0]}, Fone: ${phone}]. Detalhes: ${e.message} -- `,
         );
+
+        // Atualiza coluna se foi enviado ou nao
+        dados[index][4] = 'nao';
+
+        // Atualiza coluna se foi numero esta correto ou nao
+        if (e.message.includes('No element found for selector'))
+          dados[index][4] = 'nao';
       }
     }
 
     console.log('\n- Fim dos envios -');
     await browser.close();
+
+    // Atualiza planilha
+    const updatedWorksheet = XLSX.utils.json_to_sheet(dados);
+
+    // Criar um novo workbook com a planilha atualizada
+    const updatedWorkbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(
+      updatedWorkbook,
+      updatedWorksheet,
+      'RelCliente',
+    );
+
+    // Salvar o arquivo XLSX atualizado
+    XLSX.writeFile(updatedWorkbook, 'src/clientes/RelCliente.xlsx');
   }
 
   async delay(time: number) {
